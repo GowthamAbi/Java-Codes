0865c37bf096d19603a18460a46ab61921325eda
GowthamAbi <gowtham2131ece@gmail.com>
2025-03-10 12:22:36 +0530

    First

---
+++ Main/src/utils/Odd_Even.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Odd_Even {
+    public void check(int n) {
+        if (n % 2 == 0)
+            System.out.println(n + " is Even");
+        else
+            System.out.println(n + " is Odd");
+    }
+}
